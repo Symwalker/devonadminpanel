db161f46f8c150c89868e8d6ce397ca7345acbf7
user name <99411978+Symwalker@users.noreply.github.com>
2023-10-08 01:05:39 +0500

    first commit

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,32 @@
+
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+
+  return (
+    <>
+      <Outlet />
+    </>
+
+  )
+}
+
+export default Layout
+// <Box
+//     sx={{
+//         backgroundSize: 'contain',
+//         position:"relative",
+//         // maxHeight: '2000px',
+//         backgroundRepeat: 'repeat',
+//         // height: '2000px',
+//         backgroundImage: `url(${bg})`,
+//         // Add a background color for debugging if the image is not visible
+//         // backgroundColor: 'red',
+//     }}
+// >
+{/* <Navbar /> */ }
+
+{/* <NewsEmail/> */ }
+{/* <Footer /> */ }
+// </Box>
